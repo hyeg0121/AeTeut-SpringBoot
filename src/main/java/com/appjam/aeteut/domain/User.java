bd0f0aedc1f0e2b1a1e0ccaf03b631eaea1a9a07
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-20 18:41:51 +0900

    feat:; User Entity 추가

---
+++ src/main/java/com/appjam/aeteut/domain/User.java
@@ -0,0 +1,31 @@
+package com.appjam.aeteut.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
