16833cc3a4744527aafe407679403cba25ea9a65
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-20 18:43:06 +0900

    feat:: UserController 추가

---
+++ src/main/java/com/appjam/aeteut/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.appjam.aeteut.controller;
+
+
+import com.appjam.aeteut.dto.user.UserRequestDto;
+import com.appjam.aeteut.dto.user.UserResponseDto;
+import com.appjam.aeteut.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserResponseDto> findAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDto findAllUsers(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public UserResponseDto createUser(@RequestBody UserRequestDto userRequestDto) {
+        return userService.createUser(userRequestDto);
+    }
+
+}
