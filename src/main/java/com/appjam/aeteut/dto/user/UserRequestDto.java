74a066328f2b886f96926ab679b872e380f2f61e
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-20 18:42:50 +0900

    feat:: UserRequestDto

---
+++ src/main/java/com/appjam/aeteut/dto/user/UserRequestDto.java
@@ -0,0 +1,13 @@
+package com.appjam.aeteut.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+    private String name;
+    private String email;
+}
