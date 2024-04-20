af55fca0c76fc66eeb79bb92a3ccbdf0bd724f60
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-20 17:19:09 +0900

    init

---
+++ src/main/java/com/appjam/aeteut/AeteutApplication.java
@@ -0,0 +1,13 @@
+package com.appjam.aeteut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeteutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AeteutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/appjam/aeteut/AeteutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appjam.aeteut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeteutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
