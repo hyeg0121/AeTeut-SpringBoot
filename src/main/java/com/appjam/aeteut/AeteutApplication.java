a77668c284f46348d419ddb02d1462b6236068ac
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-20 18:43:53 +0900

    feat:: @EnableJpaAuditing 추가

--- src/main/java/com/appjam/aeteut/AeteutApplication.java
+++ src/main/java/com/appjam/aeteut/AeteutApplication.java
@@ -2,8 +2,10 @@ package com.appjam.aeteut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AeteutApplication {
 
     public static void main(String[] args) {
