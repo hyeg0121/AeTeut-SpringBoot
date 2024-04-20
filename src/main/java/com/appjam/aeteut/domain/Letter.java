44d575ea71981942ae0ebb463beaab1126506115
hyeg0121 <w2218@e-mirim.hs.kr>
2024-04-20 20:38:13 +0900

    feat::Letter Entity 추가

---
+++ src/main/java/com/appjam/aeteut/domain/Letter.java
@@ -0,0 +1,33 @@
+package com.appjam.aeteut.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Letter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String toName;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
